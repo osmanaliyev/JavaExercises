675c8ebc60a9b77a83fc604756826cdf8fc67ade
Osman Aliyev <aliyevosman52@gmail.com>
2023-09-13 18:31:15 +0400

    Java Ders 1

---
+++ Ders 1/src/Conditionals.java
@@ -0,0 +1,20 @@
+
+public class Conditionals {
+
+	public static void main(String[] args) {
+		
+		double value = 100.3;
+			if (value > 100) {
+				System.out.println("This number is bigger than hundred");
+			} 
+			else if (value == 100) {
+				System.out.println("This number is equal to 100");
+				
+			}
+			else {
+				System.out.println("This number less than hundred");
+			}
+
+	}
+
+}
---
+++ Ders 1/src/Variables.java
@@ -0,0 +1,12 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int numberOfStudents = 10;
+		String message = "Student census :";
+		
+		System.out.println(message + numberOfStudents);
+	}
+
+}
